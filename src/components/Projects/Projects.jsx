090a78eb93a9477bda6ed7fd52307cac7454d916
jmendezrf <juan_mendez1194@hotmail.com>
2020-08-05 11:43:05 -0600

    bug fixes

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
     <section id="projects">
       <Container>
         <div className="project-wrapper">
-          <Title title="technologies" />
+          <Title title="working with" />
           {projects.map((project) => {
             const { title, info, info2, url, repo, img, id } = project;
 
